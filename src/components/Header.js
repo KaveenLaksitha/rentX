8838d42ab4cfd8eb39998b69fdd69303079c3821
Kaveen LK <kmklaksitha@gmail.com>
2021-08-12 19:06:11 +0530

    styles updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -58,7 +58,7 @@ function Header(props) {
             <hr></hr>
             <li className="has-subnav">
               <li data-toggle="collapse" data-target="#new1" className="collapsed">
-                <a href="javascript:void(0)">  <i className="fa fa-clock-o fa-2x"></i> <span className="nav-text">Rentals</span> <i className="fa fa-angle-down fa-2x"></i></a>
+                <a href="javascript:void(0)">  <i className="fa fa-clock-o fa-2x"></i> <span className="nav-text">Rentals</span> <i className="fa fa-angle-right fa-animate fa-2x"></i></a>
               </li>
               <ul className="sub-menu collapse" id="new1">
                 <li className="has-subnav ">
@@ -75,7 +75,7 @@ function Header(props) {
             <hr></hr>
             <li className="has-subnav">
               <li data-toggle="collapse" data-target="#new2" className="collapsed">
-                <a href="javascript:void(0)" >  <i className="fa fa-calendar fa-2x"></i> <span className="nav-text">Reservations</span> <i className="fa fa-angle-down fa-2x"></i></a>
+                <a href="javascript:void(0)" >  <i className="fa fa-calendar fa-2x"></i> <span className="nav-text">Reservations</span> <i className="fa fa-angle-right fa-animate fa-2x"></i></a>
               </li>
               <ul className="sub-menu collapse" id="new2">
                 <li className="has-subnav ">
@@ -92,7 +92,7 @@ function Header(props) {
             <hr></hr>
             <li className="has-subnav">
               <li data-toggle="collapse" data-target="#new3" className="collapsed">
-                <a href="javascript:void(0)">  <i className="fa fa-car fa-2x"></i> <span className="nav-text">Vehicle Inventory</span> <i className="fa fa-angle-down fa-2x"></i></a>
+                <a href="javascript:void(0)">  <i className="fa fa-car fa-2x"></i> <span className="nav-text">Vehicle Inventory</span> <i className="fa fa-angle-right fa-animate fa-2x"></i></a>
               </li>
               <ul className="sub-menu collapse" id="new3">
                 <li className="has-subnav ">
@@ -115,16 +115,30 @@ function Header(props) {
             </li>
             <hr></hr>
             <li className="has-subnav">
-              <a href="/addEmployee">
-                <i className="fa fa-users fa-2x"></i>
-                <span className="nav-text">Employees</span>
-                <i className="fa fa-angle-right fa-2x"></i>
-              </a>
+              <li data-toggle="collapse" data-target="#new4" className="collapsed">
+                <a href="javascript:void(0)">  <i className="fa fa-file-pdf-o fa-2x"></i> <span className="nav-text">Employees</span> <i className="fa fa-angle-right fa-animate fa-2x"></i></a>
+              </li>
+              <ul className="sub-menu collapse" id="new4">
+                <li className="has-subnav ">
+                  <a href="/empList">
+                    <i className="fa"></i>
+                    <span className="nav-text">Employee List</span>
+                    <i className="fa fa-angle-right fa-3x"></i>
+                  </a>
+                </li>
+                <li className="has-subnav">
+                  <a href="/addEmployee">
+                    <i className="fa  fa-2x"></i>
+                    <span className="nav-text">Add New Employee</span>
+                    <i className="fa fa-angle-right fa-2x"></i>
+                  </a>
+                </li>
+              </ul>
             </li>
             <hr></hr>
             <li className="has-subnav">
               <li data-toggle="collapse" data-target="#new" className="collapsed">
-                <a href="javascript:void(0)">  <i className="fa fa-file-pdf-o fa-2x"></i> <span className="nav-text">Reports</span> <i className="fa fa-angle-down fa-2x"></i></a>
+                <a href="javascript:void(0)">  <i className="fa fa-file-pdf-o fa-2x"></i> <span className="nav-text">Reports</span> <i className="fa fa-angle-right fa-animate fa-2x"></i></a>
               </li>
               <ul className="sub-menu collapse" id="new">
                 <li className="has-subnav ">
@@ -156,8 +170,6 @@ function Header(props) {
                   </a>
                 </li>
               </ul>
-              <li>
-              </li>
             </li>
             <hr></hr>
             <li>
