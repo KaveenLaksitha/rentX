961cd999720952ab9b48eccea48544b82dba60ce
User <hasanigayani@gmail.com>
2021-08-14 00:22:14 +0530

    updated the search Function

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,7 @@ function Home() {
                 <Route path="/login" exact component={Login} />
                 <Route path="/addRental" exact component={addRental} />
                 <Route path="/rentalList" exact component={rentalList} />
-                <Route path="/updateRental" exact component={updateRental} />
+                <Route path="/updateRental/:rentalId" exact component={updateRental} />
                 <Route path="/addReservation" exact component={addReservation} />
                 <Route path="/viewReservation" exact component={viewReservation} />
                 <Route path="/updateReservation" exact component={updateReservation} />
