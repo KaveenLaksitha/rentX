7555c2dbe21afc3fad4d72bfea66310970b4f4a2
User <hasanigayani@gmail.com>
2021-08-13 23:36:10 +0530

    created the search Function

--- src/components/views/rental views/rentalList.js
+++ src/components/views/rental views/rentalList.js
@@ -10,44 +10,55 @@ function RentalList() {
 
     useEffect(() => {
 
-        /*if (document.getElementById('submit').clicked) {//this get executed if we are specifically searching
-            // searchCustomers();
+        if (document.getElementById('submit').clicked) {//this get executed if we are specifically searching
+            searchRentals();
+            /*} else if (document.getElementById('pending').clicked) {
+                pendingRecords();*/
+        } else {
+            function getRentals() {
+                axios.get("http://localhost:4000/rental/displayRentals").then((res) => { //normally the fetched rental record details are displayed through this
+                    setRentals(res.data.reverse());
+                }).catch((error) => {
+                    alert(error.message);
+                })
+            }
+            getRentals();
 
-
-
-        } else { //normally the fetched customer details are here */
-
-        function getRentals() {
-            axios.get("http://localhost:4000/rental/displayRentals").then((res) => {
-                setRentals(res.data.reverse());
-            }).catch((error) => {
-                alert(error.message);
-            })
         }
-        getRentals();
-
 
 
     }, [])
 
 
-    /*function searchCustomers(e) {
+    function pendingRecords() {
+        function getPendingRentals() {
+            axios.get("http://localhost:4000/rental/searchPendingRentalRecords/").then((res) => {
+                setRentals(res.data.reverse());
+            }).catch((error) => {
+                alert(error.message);
+            })
+        }
+        getPendingRentals();
+    }
+
+
+    function searchRentals(e) {
         e.preventDefault();
         if (!isNaN(search.charAt(0))) {//checking if the value entered at the search box is for NIC or normal name
-            axios.get(`http://localhost:8060/customer/searchCustomer/${search}`).then((res) => {
-                setCustomers(res.data);
+            axios.get(`http://localhost:4000/rental/searchRentalRecs/${search}`).then((res) => {
+                setRentals(res.data);
             }).catch((error) => {
                 alert(error.message);
             })
         } else {
 
-            axios.get(`http://localhost:8060/customer/searchCustomerByName/${search}`).then((res) => {
-                setCustomers(res.data);
+            axios.get(`http://localhost:4000/rental/searchRentalRecordsX/${search}`).then((res) => {
+                setRentals(res.data);
             }).catch((error) => {
                 alert(error.message);
             })
         }
-    }*/
+    }
 
 
     const deleteRental = async id => {
@@ -88,51 +99,62 @@ function RentalList() {
                             + Add Rental
                         </button>
                     </a>
-                    <a href="/updateRental" class="float-right ml-4">
-                        <button class="btn btn-ok white">
+                    <p class="float-right ml-4">
+                        <button class="btn btn-ok white" id="pending" onClick={pendingRecords}>
                             Pending Rental
                         </button>
-                    </a>
+                    </p>
                 </div>
                 <div class="row table-head-search">
                     <div className="col-md-8"></div>
+
                     <div className="col">
                         <div class="input-group input-group-search">
                             <div class="searchbar">
-                                <input class="search_input" type="text" name="" placeholder="Search..." />
-                                <button class="btn search_icon" type="button"><i class="fa fa-search"></i></button>
+                                <form onSubmit={searchRentals}>
+                                    <input class="search_input" type="text" name="search" placeholder="Search..."
+                                        value={search} onChange={(event) => { setSearch(event.target.value) }} require />
+                                    <button class="btn search_icon" type="submit" id="submit" name="submit"
+                                        d>
+                                        <i class="fa fa-search"></i></button>
+                                </form>
                             </div>
                         </div>
                     </div>
+
                 </div>
 
 
 
-                <table class="table table-striped table-primary">
-                    <tr>
-                        <th>From</th>
-                        <th>To</th>
-                        <th>Vehicle</th>
-                        <th>Customer</th>
-                        <th>Total</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
+                <table class="table table-hover">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>From</th>
+                            <th>To</th>
+                            <th>Vehicle</th>
+                            <th>Customer</th>
+                            <th>Total</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
                     <tbody>
                         {rentals.map((rentals) => {
                             return (
 
                                 <tr >
-                                    <td >{moment(rentals.from).format('YYYY-MMMM-DD')}</td>
+                                    <Link to={`/rental/getRentalByID/${rentals.id}`}>
+                                        <td >{moment(rentals.from).format('YYYY-MMMM-DD')}</td></Link>
                                     <td >{moment(rentals.to).format('YYYY-MMMM-DD')}</td>
                                     <td >{rentals.vehicleType}</td>
                                     <td >{rentals.customerNIC}</td>
                                     <td >{rentals.finalPrice}</td>
                                     <td >{rentals.status}</td>
                                     <td>
-                                        <Link class="btn btn-primary ml-2 fa fa-eye" to={`/rental/getRentalByID/${rentals.id}`} role="button"></Link>
-                                        <Link class="btn btn-outline-warning ml-2 fa fa-pencil-square-o" to={`/rental/updateRental/${rentals.id}`} role="button"></Link>
-                                        <Link class="fa fa-trash btn btn-danger" onClick={() => deleteRental(rentals.id)} role="button"></Link>
+
+                                        <Link class="btn btn-danger btn-sm" to={`/updateRental/${rentals.id}`} role="button"> Update</Link>
+                                        <Link class="btn btn-light btn-sm" onClick={() => deleteRental(rentals.id)} role="button"> Remove</Link>
+
                                     </td>
                                 </tr>
                             );
@@ -140,7 +162,7 @@ function RentalList() {
                     </tbody>
                 </table>
             </div>
-        </div>
+        </div >
     )
 }
 
